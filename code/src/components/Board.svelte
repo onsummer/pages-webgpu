68ccf0afb1240c47b23ca469054d7cf391b00ea9
onsummer <onsummer@foxmail.com>
2021-05-30 23:29:23 +0800

    [update] beauty the scroll bar style.

--- code/src/components/Board.svelte
+++ code/src/components/Board.svelte
@@ -37,7 +37,26 @@ function sayHi() {
     width: 35%;
     padding: 1rem;
 
-    overflow-y: scroll;
+    overflow-y: auto;
+  }
+
+  /* 滚动槽 */
+  ::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+  ::-webkit-scrollbar-track {
+    border-radius: 3px;
+    background: rgba(0, 0, 0, 0.06);
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.08);
+    -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.08);
+  }
+  /* 滚动条滑块 */
+  ::-webkit-scrollbar-thumb {
+    border-radius: 3px;
+    background: rgba(0, 0, 0, 0.12);
+    box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
+    -webkit-box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
   }
 
   .board-right {
